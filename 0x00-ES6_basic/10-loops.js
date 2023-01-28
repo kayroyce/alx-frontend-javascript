b40cd710e00767bd0e3fbf1e77943cd7236e7bc0
kayroyce <kayroyce2005@gmail.com>
2023-01-28 08:55:58 +0100

    es6lint correction

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,9 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
-	const x = [];
-	for (const idx of array) {
-		const value = idx;
-		x.push(appendString + value);
-	}
-	return x;
-}
+  const arr = [];
+  for (const idx of array) {
+    const value = idx;
+    arr.push(appendString + value);
+  }
 
+  return arr;
+}
--- 0x00-ES6_basic/6-string-interpolation.js
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -1,8 +1,12 @@
 export default function getSanFranciscoDescription() {
-	const year = 2017;
-	const budget = {
-		income: '$119,868',
-		gdp: '$154.2 billion',
-		capital: '$178,479',
-	};
-	return 'As of ${year}, it was the seventh-highest income county in the United States\ ,with a per capita personal income of ${budget.income}. As of 2015, San Francisco\ proper had a GDP of ${budget.gdp}, and aDP per capita of ${budget.capital}.';}
+  const year = 2017;
+  const budget = {
+    income: '$119,868',
+    gdp: '$154.2 billion',
+    capita: '$178,479',
+  };
+
+  return `As of ${year}, it was the seventh-highest income county in the United States\
+, with a per capita personal income of ${budget.income}. As of 2015, San Francisco \
+proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
+}
